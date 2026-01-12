37b16603c53680797aec5f1f93e44c468f16b74b
Mehul Swami <mehulswami2006@gmail.com>
2026-01-12 17:39:24 +0530

    Day 3 Question 1

---
+++ day-03-loops/print_1_to_n.py
@@ -0,0 +1,5 @@
+# Day 3 Question 1 : Write a Python program to print number from 1 to N using loops
+# Logic : Using a for Loop to execute block of code to check each number between 1 to N and print it.
+n = int(input("Enter a positive integer N: "))
+for i in range(1, n+1):
+    print(i)
